8c5b0d93764e0d4d56731d95204c9c09e95b8c64
Ignacio Viseras <ignacio.viseras@gmail.com>
2022-10-07 18:41:19 +0200

    filtro de formulario aparicion de contenidos en inputs

--- Desarrollo de Interfaces/Carrito Compra/Ignacio Viseras Riego/js/programaBase.js
+++ Desarrollo de Interfaces/Carrito Compra/Ignacio Viseras Riego/js/programaBase.js
@@ -1,26 +1,17 @@
 var nombreProducto;
 var precioArticluo;
 var cantidad;
-var total;
+var total = 0;
 var recibirLista = '';
 var listado;
 var menError;
+var menErrorPrecio;
 var botonArticulo;
-
 
  function validarFormulario(){
     filtroNombre = /^\D+$/;
     filtroPrecio1 = /^[0-9]/;
     filtroPrecio2 = /^[0-9]+.[0-9]{2}/;
-    /* if (!nombreProducto == "") {
-        if (!filtroNombre.test(nombreProducto)) {
-            menError.textContent = "tienes que poner un nombre de producto correcto";
-            return false;
-        }
-    }else{
-        menError.textContent = "Campo por rellenar";
-        return false;
-    } */
     if (nombreProducto.value.trim() == "") {
         menError.textContent = "Campo por rellenar";
         return false;
@@ -29,37 +20,39 @@ var botonArticulo;
         menError.textContent = "tienes que poner un nombre de producto correcto";
         return false;
     }
-   /*if (!precioArticluo == "") {
-        if (!filtroPrecio1.test(precioArticluo) || filtroPrecio2.test(precioArticluo)) {
-            menError.textContent = "Ejemplo: 2.45";
-            return false;
-        }
-    }else{
-        menError.textContent = "Campo por rellenar";
+    if (precioArticluo.value.trim() == "") {
+        menErrorPrecio.textContent = "Campo por rellenar"
         return false;
-    }*/
+    }
+    if (!(precioArticluo.value.match(filtroPrecio1)) && !(precioArticluo.value.match(filtroPrecio2))) {
+        menErrorPrecio.textContent = "introduce bn el numero";
+        return false;
+    }
     return true;
 }
 
 function anadir(){
     if(validarFormulario() != false){
-        console.log(nombreProducto);
         guardar();
-       /*  console.log(precioArticluo); */
     }
 }
 
 function guardar(){
     recibirLista += nombreProducto.value + ", ";
-    listado = recibirLista.substring(0, recibirLista.length-2)
-    total += precioArticluo * cantidad;
+    listado = recibirLista.substring(0, recibirLista.length-2);
+    total += precioArticluo.value * cantidad.value;
+    console.log(listado);
+    console.log(total);
+    document.getElementById("listaCarri").value = listado;
+    document.getElementById("precioCarri").value = total;
 }
 
 function inicializarVariables(){
     nombreProducto = document.getElementById("nombreProducto");
-    precioArticluo = document.getElementById("precioArticluo").value; 
-    cantidad = document.getElementById("cantidad").value;
+    precioArticluo = document.getElementById("precioArticluo"); 
+    cantidad = document.getElementById("cantidad");
     menError = document.getElementById("mensajeError");
+    menErrorPrecio = document.getElementById("mensajeErrorPrecio");
     botonArticulo = document.getElementById("añadirProductos");
 }
 
@@ -68,9 +61,6 @@ function inicializarEventos(){
 }
 
 window.onload = function(){
-    console.log("hola");
     inicializarVariables();
-    console.log("como estamosss");
     inicializarEventos();
-
 }
