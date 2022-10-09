1ee5dd243dc02f8b9e97a10f287ee07f26d0ac8e
luzlozas <78167402+luzlozas@users.noreply.github.com>
2022-10-09 21:27:39 +0200

    Cambio formato

--- Desarrollo de Interfaces/Carrito Compra/Antonio Pastor Aranda/JS/formulario.js
+++ Desarrollo de Interfaces/Carrito Compra/Antonio Pastor Aranda/JS/formulario.js
@@ -1,144 +1,120 @@
-
 //Declaracion de variables globales
 var nombre;
 var precio;
 var unidades;
-var articulos={};
+var articulos = {};
 var precioT;
 var resultado;
-var suma={};
+var suma = {};
 var disabled;
 var enabled;
-var texto="";
-var Tarjeta={};
+var texto = "";
+var Tarjeta = {};
 var numeroTarjeta;
 var nombreTitular;
 var caducidad;
 var cvc;
 
+function guardarArticulos() {
+  nombre = document.getElementById("nombre").value;
+  precio = document.getElementById("precio").value;
+  unidades = document.getElementById("unidades").value;
+  acepto = document.getElementById("acepto").value;
 
+  precioT = precio * unidades;
+  if (articulos[nombre] == null) {
+    articulos[nombre] = precioT;
+    document.getElementById("articulos").innerHTML += nombre + ", ";
+  } else {
+    articulos[nombre] += precioT;
+  }
 
-function guardarArticulos (){
-    nombre= document.getElementById("nombre").value;
-    precio= document.getElementById("precio").value;
-    unidades= document.getElementById("unidades").value;
-    acepto= document.getElementById("acepto").value;
-   
-    precioT= precio*unidades;
-    if (articulos[nombre]== null){
-        articulos[nombre]=precioT;
-        document.getElementById("articulos").innerHTML += nombre+ ", ";
-    }else{
-        articulos[nombre]+= precioT;
-    }
-
-    resultado=Object.values(articulos).join();
-        document.getElementById("precioT").innerHTML=resultado;  
-
-        
-        
+  resultado = Object.values(articulos).join();
+  document.getElementById("precioT").innerHTML = resultado;
 }
-  
+
 /*------------------------------ Control de aparición de tarjeta o efectivo ----------------------*/
- 
-    let datosTarjeta = document.getElementById("datosTarjeta");
-    const mostrar = (datosTarjeta) => {
-        datosTarjeta.style.display = 'block';
-        importeT.style.display = 'none';
-    }
-    const ocultar = (datosTarjeta) => {
-        datosTarjeta.style.display = 'none';
-        importeT.style.display = 'block';
-    }
 
+let datosTarjeta = document.getElementById("datosTarjeta");
+const mostrar = (datosTarjeta) => {
+  datosTarjeta.style.display = "block";
+  importeT.style.display = "none";
+};
+const ocultar = (datosTarjeta) => {
+  datosTarjeta.style.display = "none";
+  importeT.style.display = "block";
+};
 
-    function mostrarT(datosTarjeta) { 
-        let tarjeta = document.getElementById(datosTarjeta);
-        window.getComputedStyle(tarjeta).display !== 'none';
-            mostrar(tarjeta);
-    }
+function mostrarT(datosTarjeta) {
+  let tarjeta = document.getElementById(datosTarjeta);
+  window.getComputedStyle(tarjeta).display !== "none";
+  mostrar(tarjeta);
+}
 
-    function ocultarT(datosTarjeta) { 
-        let tarjeta = document.getElementById(datosTarjeta);
-        window.getComputedStyle(tarjeta).display == 'block'; 
-            ocultar(tarjeta);
-    }
+function ocultarT(datosTarjeta) {
+  let tarjeta = document.getElementById(datosTarjeta);
+  window.getComputedStyle(tarjeta).display == "block";
+  ocultar(tarjeta);
+}
 
 /*-------------------------------Reseteo de formulario---------------------------------*/
-    
+
 function limpiarFormulario() {
-    document.getElementById("form").reset();
-    document.getElementById("importe").value = "";
-    document.getElementById("precioT").value = "";
-    document.getElementById("articulos").value = "";
-
-
-
+  document.getElementById("form").reset();
+  document.getElementById("importe").value = "";
+  document.getElementById("precioT").value = "";
+  document.getElementById("articulos").value = "";
 }
-
 
 /*-------------------------------Sumatorio de efectivo---------------------------------*/
-    
-function sumatorioEfectivo() {
-    suma= Object.values(articulos).reduce((a, b) => a + b, 0);
-    document.getElementById("importe").innerHTML=suma;
-     
-}
 
+function sumatorioEfectivo() {
+  suma = Object.values(articulos).reduce((a, b) => a + b, 0);
+  document.getElementById("importe").innerHTML = suma;
+}
 
 /*-------------------------------Bloqueo imprimir---------------------------------*/
-    
+
 function bloqueoImprimir() {
-   
-    document.getElementById("imprimir").enabled=true;
+  document.getElementById("imprimir").enabled = true;
 }
 
-function desbloqueoImprimir(){
-            
-    document.getElementById("imprimir").disabled=true;
+function desbloqueoImprimir() {
+  document.getElementById("imprimir").disabled = true;
 }
 
-function controlImprimir(){
-
-    if(document.getElementById("acepto").checked==true){
-        document.getElementById("imprimir").disabled=false;
-
-    } else{
-        document.getElementById("imprimir").disabled=true;
-
-    }
-} 
+function controlImprimir() {
+  if (document.getElementById("acepto").checked == true) {
+    document.getElementById("imprimir").disabled = false;
+  } else {
+    document.getElementById("imprimir").disabled = true;
+  }
+}
 
 /*--------------------------------Carga de ventana---------------------------------*/
 
-window.onload = function() {
-
-      
-    document.getElementById("botonArticulos").addEventListener("click", guardarArticulos);
-    document.getElementById("tarjeta").addEventListener("click", mostrarT);
-    document.getElementById("efectivo").addEventListener("click", sumatorioEfectivo);
-    document.getElementById("acepto").addEventListener("click", controlImprimir);
-    document.getElementById("botonArticulos").addEventListener("click", Validar);
-    document.getElementById("restablecer").addEventListener("click", limpiarFormulario);
-}
-
-
-
-
+window.onload = function () {
+  document
+    .getElementById("botonArticulos")
+    .addEventListener("click", guardarArticulos);
+  document.getElementById("tarjeta").addEventListener("click", mostrarT);
+  document
+    .getElementById("efectivo")
+    .addEventListener("click", sumatorioEfectivo);
+  document.getElementById("acepto").addEventListener("click", controlImprimir);
+  document.getElementById("botonArticulos").addEventListener("click", Validar);
+  document
+    .getElementById("restablecer")
+    .addEventListener("click", limpiarFormulario);
+};
 
 /*-----------------------------------Validación JS-------------------- */
 
-function Validar(){
+function Validar() {
+  //validamos el nombre solo con letras y que admita compuestos
 
-    //validamos el nombre solo con letras y que admita compuestos
-
-    if (nombre.trim().match(/^[a-zA-Z]+$/)==null) {
-        alert('[ERROR]--Introduce el nombre correctamente.')
-        return false;
-    }
-
-   
-   
-    
-    
+  if (nombre.trim().match(/^[a-zA-Z]+$/) == null) {
+    alert("[ERROR]--Introduce el nombre correctamente.");
+    return false;
+  }
 }
--- Desarrollo de Interfaces/Carrito Compra/Ignacio Viseras Riego/js/programaBase.js
+++ Desarrollo de Interfaces/Carrito Compra/Ignacio Viseras Riego/js/programaBase.js
@@ -2,65 +2,68 @@ var nombreProducto;
 var precioArticluo;
 var cantidad;
 var total = 0;
-var recibirLista = '';
+var recibirLista = "";
 var listado;
 var menError;
 var menErrorPrecio;
 var botonArticulo;
 
- function validarFormulario(){
-    filtroNombre = /^\D+$/;
-    filtroPrecio1 = /^[0-9]/;
-    filtroPrecio2 = /^[0-9]+.[0-9]{2}/;
-    if (nombreProducto.value.trim() == "") {
-        menError.textContent = "Campo por rellenar";
-        return false;
-    }
-    if (!nombreProducto.value.match(filtroNombre)) {
-        menError.textContent = "tienes que poner un nombre de producto correcto";
-        return false;
-    }
-    if (precioArticluo.value.trim() == "") {
-        menErrorPrecio.textContent = "Campo por rellenar"
-        return false;
-    }
-    if (!(precioArticluo.value.match(filtroPrecio1)) && !(precioArticluo.value.match(filtroPrecio2))) {
-        menErrorPrecio.textContent = "introduce bn el numero";
-        return false;
-    }
-    return true;
+function validarFormulario() {
+  filtroNombre = /^\D+$/;
+  filtroPrecio1 = /^[0-9]/;
+  filtroPrecio2 = /^[0-9]+.[0-9]{2}/;
+  if (nombreProducto.value.trim() == "") {
+    menError.textContent = "Campo por rellenar";
+    return false;
+  }
+  if (!nombreProducto.value.match(filtroNombre)) {
+    menError.textContent = "tienes que poner un nombre de producto correcto";
+    return false;
+  }
+  if (precioArticluo.value.trim() == "") {
+    menErrorPrecio.textContent = "Campo por rellenar";
+    return false;
+  }
+  if (
+    !precioArticluo.value.match(filtroPrecio1) &&
+    !precioArticluo.value.match(filtroPrecio2)
+  ) {
+    menErrorPrecio.textContent = "introduce bn el numero";
+    return false;
+  }
+  return true;
 }
 
-function anadir(){
-    if(validarFormulario() != false){
-        guardar();
-    }
+function anadir() {
+  if (validarFormulario() != false) {
+    guardar();
+  }
 }
 
-function guardar(){
-    recibirLista += nombreProducto.value + ", ";
-    listado = recibirLista.substring(0, recibirLista.length-2);
-    total += precioArticluo.value * cantidad.value;
-    console.log(listado);
-    console.log(total);
-    document.getElementById("listaCarri").value = listado;
-    document.getElementById("precioCarri").value = total;
+function guardar() {
+  recibirLista += nombreProducto.value + ", ";
+  listado = recibirLista.substring(0, recibirLista.length - 2);
+  total += precioArticluo.value * cantidad.value;
+  console.log(listado);
+  console.log(total);
+  document.getElementById("listaCarri").value = listado;
+  document.getElementById("precioCarri").value = total;
 }
 
-function inicializarVariables(){
-    nombreProducto = document.getElementById("nombreProducto");
-    precioArticluo = document.getElementById("precioArticluo"); 
-    cantidad = document.getElementById("cantidad");
-    menError = document.getElementById("mensajeError");
-    menErrorPrecio = document.getElementById("mensajeErrorPrecio");
-    botonArticulo = document.getElementById("añadirProductos");
+function inicializarVariables() {
+  nombreProducto = document.getElementById("nombreProducto");
+  precioArticluo = document.getElementById("precioArticluo");
+  cantidad = document.getElementById("cantidad");
+  menError = document.getElementById("mensajeError");
+  menErrorPrecio = document.getElementById("mensajeErrorPrecio");
+  botonArticulo = document.getElementById("añadirProductos");
 }
 
-function inicializarEventos(){
-    document.getElementById("añadirProductos").addEventListener("click", anadir);
+function inicializarEventos() {
+  document.getElementById("añadirProductos").addEventListener("click", anadir);
 }
 
-window.onload = function(){
-    inicializarVariables();
-    inicializarEventos();
-}
+window.onload = function () {
+  inicializarVariables();
+  inicializarEventos();
+};
