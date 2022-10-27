24f11eec849f85c54bde69804ff31640d72cb783
ergoegos <ergoegos@gmail.com>
2022-10-27 23:22:46 +0200

    connection client-server works

--- Programacion de Servicios y Procesos/A1-Hilos y Sockets/server/src/main/java/server/Server.java
+++ Programacion de Servicios y Procesos/A1-Hilos y Sockets/server/src/main/java/server/Server.java
@@ -5,15 +5,13 @@ import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
-import java.util.Iterator;
 
 import beans.Book;
 
 public class Server {
 
-	
 	private static final int PORT=2000;
-	
+	private static final String IP_SERVER="localhost";
 	public static void main(String[] args) {
 		
 		ArrayList<Book> books = new ArrayList();
@@ -28,20 +26,23 @@ public class Server {
 		
 		System.out.println("APLICACIÓN DE SERVIDOR MULTITAREA");
 		System.out.println("----------------------------------");
-		try {
-			
-			ServerSocket server = new ServerSocket();
-			// InetSocketAddress direction = new InetSocketAddress("ip public",PORT);
-			InetSocketAddress direction = new InetSocketAddress(PORT); //localhost
+		
+		// initialize server inet socket
+		try (ServerSocket serverSocket = new ServerSocket()){
+			InetSocketAddress direction = new InetSocketAddress(IP_SERVER,PORT); 
+			serverSocket.bind(direction);
 
-			server.bind(direction);
-
-			System.out.println("Servidor listo para aceptar solicitudes");
-			System.out.println("Dirección IP: " + direction.getAddress());
 			
-			while (true) {
-				Socket socket = server.accept();
-				System.out.println("Comunicación entrante");
+			System.out.println("INET: " + direction.getHostName());
+			System.out.println("PORT: " + direction.getPort());
+			System.out.println("IP: " + direction.getAddress());
+			
+			System.out.println("Server socket ready to listen petitions...");
+			
+			// client exist 
+			while (true) {	
+				Socket socket = serverSocket.accept();
+				System.out.println("["+ direction.getAddress() +"]");
 				new ServerThread(socket, "thread-"+direction.getHostName());
 			}
 			
--- Programacion de Servicios y Procesos/A1-Hilos y Sockets/server/src/main/java/server/ServerThread.java
+++ Programacion de Servicios y Procesos/A1-Hilos y Sockets/server/src/main/java/server/ServerThread.java
@@ -1,9 +1,17 @@
 package server;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.PrintStream;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import beans.Book;
 
 public class ServerThread extends Thread {
 
@@ -13,15 +21,18 @@ public class ServerThread extends Thread {
 	private Socket socket;
 	private String name;
 	private long born;
+	private Book book;
+	
+	private List<String> data;
 
 	public ServerThread(Socket socket, String name) {
 
 		id++;
 
 		this.socket = socket;
-		this.name = name;
+		this.name = name + "-" + id;
 		this.born = System.currentTimeMillis();
-
+		
 		thread = new Thread(this, "Cliente-" + id);
 		thread.start();
 	}
@@ -31,26 +42,55 @@ public class ServerThread extends Thread {
 		System.out.println("Estableciendo comunicación con " + thread.getName());
 		try {
 
-			InputStream input = socket.getInputStream();
-			OutputStream output = socket.getOutputStream();
-			String data = "";
+			//InputStream input = socket.getInputStream();
+			PrintStream output = new PrintStream(socket.getOutputStream());
+			InputStreamReader input = new InputStreamReader(socket.getInputStream());
+			BufferedReader bf = new BufferedReader(input);
 
-			while (!data.trim().equals("FIN")) {
-				
-				byte[] mensaje = new byte[100];
-				input.read(mensaje);
-				data = new String(mensaje);
-				
-				
-				if (data.trim().equals("FIN")) {
-					output.write("Hasta pronto, gracias por establecer conexión".getBytes());
-					System.out.println(thread.getName() + " ha cerrado la comunicación. ");
+			
+			//El servidor se quedaría aquí parado hasta que el cliente nos mande
+			//informacion, es decir, cuando haga un salida.println(INFORMACION);				
+			setData(bf.readLine());
+			
+			System.out.println("Client "+id+" - " + data);
+			
+			while (!data.get(0).equalsIgnoreCase("0")) {
 					
-				} else {
-					System.out.println(thread.getName() + " dice: " + data);
-					output.write(("Tu mensaje tiene " + data.trim().length() + " caracteres").getBytes());
+				if (data.get(0).equalsIgnoreCase("1")) {
+					// consultar por isbn
+					
+					System.out.println(data.get(1));
+					
+					output.println("funcionaaaaaaaaaaaaa"); // envia la info al cliente
 				}
+				
+				if (data.get(0).equalsIgnoreCase("2")) {
+					// consultar por titulo
+					
+					output.println(book.toString());
+					
+				}
+				if (data.get(0).equalsIgnoreCase("3")) {
+					// consultar por autor
+					
+					output.println(book.toString());
+				}
+				
+				if (data.get(0).equalsIgnoreCase("4")) {
+					// añadir libro
+					
+					
+					output.println(book.toString());
+				}
+				
+				
+				setData(bf.readLine());
+				
 			}
+			
+			output.write("Hasta pronto, gracias por establecer conexión".getBytes());
+			System.out.println(thread.getName() + " ha cerrado la comunicación. ");
+					
 			
 			input.close();
 			output.close();
@@ -59,7 +99,15 @@ public class ServerThread extends Thread {
 		} catch (IOException e) {
 			System.out.println(e.getMessage());
 		} 
-
 	}
 
+	public List<String> getData() {
+		return data;
+	}
+
+	public void setData(String string) {
+		this.data = Arrays.asList(string.split("-"));
+	}
+	
+	
 }
