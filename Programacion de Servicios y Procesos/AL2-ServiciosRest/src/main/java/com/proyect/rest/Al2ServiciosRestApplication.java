134a73fffb626956199ef35e8cf6886bde041780
antoprog <anto.programacion@gmail.com>
2022-11-12 11:07:27 +0100

    Subida AL2 a git
    Cambio de proyecto por mala subida anterior

---
+++ Programacion de Servicios y Procesos/AL2-ServiciosRest/src/main/java/com/proyect/rest/Al2ServiciosRestApplication.java
@@ -0,0 +1,13 @@
+package com.proyect.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Al2ServiciosRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Al2ServiciosRestApplication.class, args);
+	}
+
+}
