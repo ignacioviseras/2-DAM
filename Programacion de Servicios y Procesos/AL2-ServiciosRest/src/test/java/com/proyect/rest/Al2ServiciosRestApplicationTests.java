2fe0295d5070830c0b69f2ee9a1821e501020e09
PC_ignacioviseras <ignacio.viseras@gmail.com>
2022-11-21 12:21:25 +0100

    Añadiendo actividad felix falta que el menu funcione sin dar errores

---
+++ Programacion de Servicios y Procesos/AL2-ServiciosRest/src/test/java/com/proyect/rest/Al2ServiciosRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyect.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Al2ServiciosRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
