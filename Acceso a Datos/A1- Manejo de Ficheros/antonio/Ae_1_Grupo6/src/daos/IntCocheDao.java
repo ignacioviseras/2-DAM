625a9baede8f1dcbeef4f75a7074fbfa9c65441d
antoprog <anto.programacion@gmail.com>
2023-01-17 12:50:28 +0100

    actualiz act opcion uno
    la opcion uno funciona pero peta al volver al menu

---
+++ Acceso a Datos/A1- Manejo de Ficheros/antonio/Ae_1_Grupo6/src/daos/IntCocheDao.java
@@ -0,0 +1,17 @@
+package daos;
+
+import java.util.ArrayList;
+
+
+import beans.Coche;
+
+public interface IntCocheDao {
+	
+	public int newCar (Coche coche);
+	public boolean deleteCar (int id);	
+	public Coche findById (int id);
+	public ArrayList<Coche> listaCoches ();
+	
+	
+
+}
